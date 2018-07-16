dfdce0aee8c2bf5c9425cafcdbeee2cef5b3df1a
dam9it <dam9it@gmail.com>
2018-07-16 18:08:41 +0300

    Add files via upload

---
+++ KoodiTesti/src/main/java/Main.java
@@ -0,0 +1,28 @@
+import static spark.Spark.*;
+
+import java.io.Console;
+
+import org.apache.commons.codec.digest.DigestUtils;
+import org.json.*;
+import org.json.simple.JSONObject;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        post("/hash", "application/json", (req,res) -> {
+        	String[] parts = req.body().split("\"");
+        	String[] parts2 = parts[2].split("-");
+        	String lines[] = parts2[0].split("\\r?\\n");
+
+        	String input = lines[2];
+        	String sha256hex = DigestUtils.sha256Hex(input);
+        	
+        	JSONObject obj = new JSONObject();
+        	  obj.put("hash", sha256hex);
+        	  obj.toString(); 
+        	
+        	return obj;
+    
+        	});
+    }
+}
